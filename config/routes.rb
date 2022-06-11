7e9871ae98a2002d6ab8f49de326fb90e37561c2
Przemysław Prencel <hartag96@gmail.com>
2022-06-11 18:24:09 +0200

    root level redirect to projects page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: "projects#index" # tempomary walk around
   resources :jewels
   resources :projects
   get 'home/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
